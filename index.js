64d923eaac87a635689a4d8d76e1e413e8b4e739
masandrii <46366767+masandrii@users.noreply.github.com>
2019-04-20 08:05:44 +0700

    upload data

---
+++ index.js
@@ -0,0 +1,57 @@
+//declaration of constansta and all libraty using in project
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const mysql = require('mysql');
+ 
+// parse application/json
+app.use(bodyParser.json());
+ 
+//create database connection
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'datacenter'
+});
+ 
+//connect to database
+conn.connect((err) =>{
+  if(err) throw err;
+  console.log('Mysql Connected...');
+});
+ 
+//insert data param string
+app.post('/api/message/:string',(req, res) => {
+  let data = {message: req.params.string};
+  let sql = "INSERT INTO production SET ?"; 
+  let query = conn.query(sql, data,(err, results) => {
+    if(err) throw err; // detect if error case
+    res.send(JSON.stringify({"status": "success","insert_data": req.params.string , "response": results})); //response
+  });
+});
+
+  //select all data production
+app.get('/api/production',(req, res) => {
+  let sql = "SELECT * FROM production";
+  let query = conn.query(sql, (err, results) => {
+    if(err) throw err;
+    res.send(JSON.stringify({"status": "success", "response": results}));
+  });
+});
+
+//select data production by id
+app.get('/api/production/:id',(req, res) => {
+  let sql = "SELECT * FROM production WHERE id="+req.params.id;
+  let query = conn.query(sql, (err, results) => {
+    if(err) throw err;
+    res.send(JSON.stringify({"status": "success", "response": results}));
+  });
+});
+
+
+ 
+
+app.listen(3838,() =>{ // running server using port 3838
+    console.log('Server started on port 3838...');
+  });
